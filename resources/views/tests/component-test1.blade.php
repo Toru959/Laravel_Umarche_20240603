391fe54a4da1f074d5b4ed4f98f00e168058f66d
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-06-05 21:07:53 +0800

    Attribute Bag

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -4,5 +4,7 @@ component_test1
 
     <x-tests.card title="タイトル" content="本文" :message="$message" />
     <x-tests.card title="タイトル2" />
+    <x-tests.card title="CSSを変更したい" class="bg-red-300" />
+    
 </x-tests.app>
 

beb8498c32f4845f3993f278133dae1d20a7b67a
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-06-05 21:35:01 +0800

    ClassBase

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -5,6 +5,7 @@ component_test1
     <x-tests.card title="タイトル" content="本文" :message="$message" />
     <x-tests.card title="タイトル2" />
     <x-tests.card title="CSSを変更したい" class="bg-red-300" />
-    
-</x-tests.app>
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更しています" />
+    </x-tests.app>
 

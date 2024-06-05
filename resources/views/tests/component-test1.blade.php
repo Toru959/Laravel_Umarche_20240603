da2c20d4d7f875e26e10642ebd2a92288c049039
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-06-05 10:24:45 +0800

    Variable

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,6 +2,6 @@
     <x-slot name="header">header1</x-slot>
 component_test1
 
-    <x-tests.card title="タイトル" content="本文" />
+    <x-tests.card title="タイトル" content="本文" :message="$message" />
 </x-tests.app>
 

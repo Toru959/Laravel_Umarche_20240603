a436b874fa9cad4f36f7736afba4b232e2703044
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-06-10 01:56:10 +0800

    Carbon

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Owner; //Eloquent
+use Illuminate\Support\Facades\DB; // QueryBuilder
+use Carbon\Carbon;
 
 class OwnersController extends Controller
 {
@@ -18,11 +21,25 @@ class OwnersController extends Controller
         $this->middleware('auth:admin');
     }
        
-
      
     public function index()
     {
-        dd('確認中');     
+        $data_now = Carbon::now();
+        $data_parse = Carbon::parse(now());
+        echo $data_now;
+        echo $data_parse;
+
+        $e_all = Owner::all();
+        $q_get = DB::table('owners')->select('name', 'created_at')->get();
+        //$q_first = DB::table('owners')->select('name')->first();
+
+        //$c_test = collect([
+        //    'name' => 'テスト'
+        //]);
+
+        //var_dump($q_first);
+        //dd($e_all, $q_get, $q_first, $c_test);     
+        return view('admin.owners.index', compact('e_all', 'q_get'));
     }
 
     /**

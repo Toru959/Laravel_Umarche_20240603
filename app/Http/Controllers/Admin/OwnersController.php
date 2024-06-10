0501c02e1639a66429d3dd2b596ae494d068358f
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-06-10 20:13:35 +0800

    index, create

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -24,22 +24,8 @@ class OwnersController extends Controller
      
     public function index()
     {
-        $data_now = Carbon::now();
-        $data_parse = Carbon::parse(now());
-        echo $data_now;
-        echo $data_parse;
-
-        $e_all = Owner::all();
-        $q_get = DB::table('owners')->select('name', 'created_at')->get();
-        //$q_first = DB::table('owners')->select('name')->first();
-
-        //$c_test = collect([
-        //    'name' => 'テスト'
-        //]);
-
-        //var_dump($q_first);
-        //dd($e_all, $q_get, $q_first, $c_test);     
-        return view('admin.owners.index', compact('e_all', 'q_get'));
+        $owners = Owner::select('name', 'email', 'created_at')->get();
+        return view('admin.owners.index', compact('owners'));
     }
 
     /**
@@ -49,7 +35,7 @@ class OwnersController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.owners.create');
     }
 
     /**

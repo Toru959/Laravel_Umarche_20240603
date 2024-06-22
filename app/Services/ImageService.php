9324fdb044ffe977a29b54b0eae7630b52cca830
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-06-22 08:55:38 +0800

    store

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -5,12 +5,19 @@ use Illuminate\Support\Facades\Storage;
 use InterventionImage;
 
 class ImageService{
-    public static function upload($imageFile, $folderName){
+    public static function upload($imageFile, $folderName){ 
+        //dd($imageFile['image']);
+        if(is_array($imageFile)){
+            $file = $imageFile['image'];
+        } else {
+            $file = $imageFile;
+        }
+
         $fileName = uniqid(rand().'_');
-        $extension = $imageFile->extension();
+        $extension = $file->extension();
         $fileNameToStore = $fileName.'.'.$extension;
 
-        Storage::putFileAs('public/'.$folderName.'/', $imageFile, $fileNameToStore);
+        Storage::putFileAs('public/'.$folderName.'/', $file, $fileNameToStore);
         //Storage::putFile('public/'.$folderName.'/'. $imageFile); //リサイズなしの場合
         return $fileNameToStore;
     }

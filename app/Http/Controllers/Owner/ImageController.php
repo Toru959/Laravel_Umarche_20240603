c1155b5f274a4046aa4efbae08f5fdc2a0a6e051
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-06-22 14:16:58 +0800

    edit, update

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -93,7 +93,8 @@ class ImageController extends Controller
      */
     public function edit($id)
     {
-        //
+        $image = Image::findOrFail($id);
+        return view('owner.images.edit', compact('image'));
     }
 
     /**
@@ -105,7 +106,18 @@ class ImageController extends Controller
      */
     public function update(Request $request, $id)
     {
-       // 
+        $request->validate([
+            'title' => 'string|max:50',
+        ]);
+
+        $image = Image::findOrFail($id);
+        $image->title = $request->title;
+        
+        $image->save();
+        
+        return redirect()
+        ->route('owner.images.index')
+        ->with(['message' => '画像情報を更新しました。', 'status' => 'info']);  // 
     }
 
     /**

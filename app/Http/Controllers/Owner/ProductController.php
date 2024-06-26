9ecc7258929097916ea1c119c90d40cfbf1549ad
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-06-27 03:02:36 +0800

    productDelete

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -221,6 +221,10 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $product = Product::findOrFail($id)->delete();
+
+        return redirect()
+        ->route('owner.products.index')
+        ->with(['message' => '商品を削除しました。', 'status' => 'alert']);
     }
 }

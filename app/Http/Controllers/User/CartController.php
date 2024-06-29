bdb9f9b1a9c8f7c876437546f674c84da6f0b0cd
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-06-29 15:05:59 +0800

    cart index

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -5,27 +5,42 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Cart;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
-{
+{   
+    public function index()
+    {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+        $totalPrice = 0;
+
+        foreach($products as $product){
+            $totalPrice = $product->price * $product->pivot->quantity;
+        }
+
+        return view('user.cart', compact('products', 'totalPrice'));
+    }
+
     public function add(Request $request)
     {
-        $itemInCart = Cart::where('product_id', $request->product_id)
+        $itemInCart = Cart::where('product_id', $request->product_id) //　カートに商品があるか確認
         ->where('user_id', Auth::id())
         ->first();
 
         if($itemInCart){
-            $itemInCart->quantity += $request->quantity;
+            $itemInCart->quantity += $request->quantity; // あれば数量を追加
             $itemInCart->save();
 
         } else {
-            Cart::create([
+            Cart::create([ // なければ新規作成
                 'user_id' => Auth::id(),
                 'product_id' => $request->product_id,
                 'quantity' => $request->quantity,
             ]);
         }
-        dd('テスト');
+        
+        return redirect()->route('user.cart.index');
     } 
 }
--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,7 @@ Route::middleware('auth:users') //userとしてログインしてた場合
 Route::prefix('cart')
 ->middleware('auth:users')
 ->group(function(){
+    Route::get('/', [CartController::class, 'index'])->name('cart.index');
     Route::post('/add', [CartController::class, 'add'])->name('cart.add');
 });
 

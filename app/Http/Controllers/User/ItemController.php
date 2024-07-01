a8fcee3566542551d955e9b769ad48b1c90b2427
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-07-01 08:17:29 +0800

    キュー

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -10,6 +10,7 @@ use App\Models\Stock;
 use App\Models\PrimaryCategory;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\TestMail;
+use App\Jobs\SendThanksMail;
 
 class ItemController extends Controller
 {
@@ -20,9 +21,11 @@ class ItemController extends Controller
 
    public function index(Request $request)
    {
-        Mail::to('test@example.com')
-        ->send(new TestMail());
-        
+        //Mail::to('test@example.com')
+        //->send(new TestMail());
+
+        SendThanksMail::dispatch();
+
         $categories = PrimaryCategory::with('secondary')
         ->get();
 
